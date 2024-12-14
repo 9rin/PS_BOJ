53b9e47e1df23cc2be536eda99569443f4094624
9rin <129544574+9rin@users.noreply.github.com>
2024-12-14 23:43:40 +0900

    Add files via upload

---
+++ Bronze/[B4] 30402번 감마선을 맞은 컴퓨터.py
@@ -0,0 +1,11 @@
+for i in range(15):
+    arr = str(input())
+    if "w" in arr:
+        print("chunbae")
+        break
+    elif "b" in arr:
+        print("nabi")
+        break
+    elif "g" in arr:
+        print("yeongcheol")
+        break
